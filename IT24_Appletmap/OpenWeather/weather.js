eb2a620115c2804f1731746e16ca1a7932ed4018
rouelamaejonayon <jonayonrouelamae@gmail.com>
2024-11-02 20:57:39 +0900

    created a class

---
+++ IT24_Appletmap/OpenWeather/weather.js
@@ -0,0 +1,24 @@
+class WeatherApp {
+    constructor() {
+        this.apiKeyInput = document.getElementById('apiKeyInput');
+        this.cityInput = document.getElementById('cityInput');
+        this.getWeatherBtn = document.getElementById('getWeatherBtn');
+        this.getLocationBtn = document.getElementById('getLocationBtn');
+        this.weatherCard = document.getElementById('weatherCard');
+        this.cityName = document.getElementById('cityName');
+        this.temperature = document.getElementById('temperature');
+        this.description = document.getElementById('description');
+        this.humidity = document.getElementById('humidity');
+        this.windSpeed = document.getElementById('windSpeed');
+        this.weatherIcon = document.getElementById('weatherIcon');
+        this.map = L.map('map').setView([0, 0], 2); 
+
+        
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap'
+        }).addTo(this.map);
+        this.getWeatherBtn.addEventListener('click', () => this.fetchWeather());
+        this.getLocationBtn.addEventListener('click', () => this.fetchWeatherByLocation());
+
+    }

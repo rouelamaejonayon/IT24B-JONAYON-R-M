f07d9aa3aea111a5fa091f716d58533761447e0f
rouelamaejonayon <jonayonrouelamae@gmail.com>
2024-11-07 09:49:21 +0900

    polished

--- IT24_Appletmap/AppletMap/appletmap.js
+++ IT24_Appletmap/AppletMap/appletmap.js
@@ -8,7 +8,7 @@ class LeafletMap {
     initTileLayer() {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.map);
     }
 
